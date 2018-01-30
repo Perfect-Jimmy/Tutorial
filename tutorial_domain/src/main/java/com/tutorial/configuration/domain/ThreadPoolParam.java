8dd77f4e752e4e14aff0d9ef2e8cb17a84df2f53
jimmy <773792867@qq.com>
2018-01-30 17:17:26 +0800

    jest

---
+++ tutorial_domain/src/main/java/com/tutorial/configuration/domain/ThreadPoolParam.java
@@ -0,0 +1,18 @@
+package com.tutorial.configuration.domain;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Jimmy. 2018/1/30  17:12
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix="spring.task.pool")
+public class ThreadPoolParam {
+    private int corePoolSize;
+    private int maxPoolSize;
+    private int keepAliveSeconds;
+    private int queueCapacity;
+}
---
+++ tutorial_springboot/src/main/java/com/tutorial/configuration/ThreadPoolConfiguration.java
@@ -0,0 +1,35 @@
+package com.tutorial.configuration;
+
+import com.tutorial.configuration.domain.ThreadPoolParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * Created by Jimmy. 2018/1/30  17:09
+ */
+@Component
+public class ThreadPoolConfiguration {
+    @Autowired
+    private ThreadPoolParam threadPoolParam;
+
+    @Bean
+    public ThreadPoolTaskExecutor taskExecutor(){
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(threadPoolParam.getCorePoolSize());
+        executor.setMaxPoolSize(threadPoolParam.getMaxPoolSize());
+        executor.setQueueCapacity(threadPoolParam.getQueueCapacity());
+        executor.setKeepAliveSeconds(threadPoolParam.getKeepAliveSeconds());
+        // executor.setAllowCoreThreadTimeOut(true);
+        executor.setThreadNamePrefix("MyExecutor-");
+        // rejection-policy：当pool已经达到max size的时候，如何处理新任务
+        // CALLER_RUNS：主线程直接执行该任务，执行完之后尝试添加下一个任务到线程池中，可以有效降低向线程池内添加任务的速度
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.initialize();
+        System.out.println(executor);
+        return executor;
+    }
+}
