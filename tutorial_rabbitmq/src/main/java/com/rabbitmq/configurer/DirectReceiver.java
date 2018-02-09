97ea4c8a90562b9e4dc239fdfc305651caac0594
jimmy <773792867@qq.com>
2018-02-10 00:43:59 +0800

    rabbitmq

--- tutorial_rabbitmq/src/main/java/com/rabbitmq/configurer/DirectReceiver.java
+++ tutorial_rabbitmq/src/main/java/com/rabbitmq/configurer/DirectReceiver.java
@@ -1,12 +1,15 @@
 package com.rabbitmq.configurer;
 
 import com.rabbitmq.Animal;
+import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -23,9 +26,12 @@ public class DirectReceiver {
     }*/
 
     @RabbitListener(queues = "direct_queue")
-    public String process2(@Payload Animal animal) throws InterruptedException {
+    public String process2(@Payload Animal animal, Channel channel, Message message) throws InterruptedException, IOException {
         LOGGER.info("接收到direct_queue的消息：" + animal);
-        TimeUnit.SECONDS.sleep(30);
+        TimeUnit.SECONDS.sleep(10);
+        LOGGER.info("消息Id {}",message.getMessageProperties().getDeliveryTag());
+        // 确认消息已经消费成功
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
         return animal.toString();
     }
 }
--- tutorial_rabbitmq/src/main/java/com/rabbitmq/configurer/DirectSender.java
+++ tutorial_rabbitmq/src/main/java/com/rabbitmq/configurer/DirectSender.java
@@ -30,7 +30,12 @@ public class DirectSender implements RabbitTemplate.ConfirmCallback, RabbitTempl
         rabbitTemplate.setReturnCallback(this);
     }
 
-    //消息回调只能代表成功消息发送到RabbitMQ服务器,不能代表消息被成功处理和接受
+    /**
+     * 当消息成功到达exchange的时候触发的ack回调
+     * @param correlationData
+     * @param ack
+     * @param cause
+     */
     @Override
     public void confirm(CorrelationData correlationData, boolean ack, String cause) {
         if (ack) {
@@ -41,6 +46,16 @@ public class DirectSender implements RabbitTemplate.ConfirmCallback, RabbitTempl
 
     }
 
+    /**
+     * 当消息成功到达exchange,但是没有队列与之绑定的时候触发的ack回调
+     * 用于实现消息发送到RabbitMQ交换器,但无相应队列与交换器绑定时的回调.
+     * 基本上来说线程不可能出现这种情况,除非手动将已经存在的队列删掉,否则在测试阶段肯定能测试出来.
+     * @param message
+     * @param replyCode
+     * @param replyText
+     * @param exchange
+     * @param routingKey
+     */
     @Override
     public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
         LOGGER.info("消息发送失败:{}",message.getMessageProperties().getCorrelationIdString());
@@ -57,4 +72,10 @@ public class DirectSender implements RabbitTemplate.ConfirmCallback, RabbitTempl
         LOGGER.info("结束发送消息:{}",object);
         LOGGER.info("消费者响应:{},消息处理完成",response);
     }
+
+   /* rabbitTemplate.send(message); //发消息,参数类型为org.springframework.amqp.core.Message
+      rabbitTemplate.convertAndSend(object); //转换并发送消息.将参数对象转换为org.springframework.amqp.core.Message后发送.
+      这个是异步的.消息是否发送成功需要用到ConfirmCallback和ReturnCallback回调函数类确认.
+      rabbitTemplate.convertSendAndReceive(message)//转换并发送消息,且等待消息者返回响应消息.
+      这是一个RPC方法,当发送消息过后,该方法会一直阻塞在哪里等待返回结果,直到请求超时.可以通过配置spring.rabbitmq.template.reply-timeout来配置超时时间*/
 }
