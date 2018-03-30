533b39f888bea3b93131150b9632374d117f514c
jimmy <773792867@qq.com>
2018-03-30 17:52:18 +0800

    spring boot 2.0 AOP日志

---
+++ tutorial_springboot/src/main/java/com/tutorial/configurer/CustomerWebMvcConfigurer.java
@@ -0,0 +1,53 @@
+package com.tutorial.configurer;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.tutorial.interceptor.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * Created by Jimmy. 2018/3/30  16:
+ * WebMvcConfigurer 配置 拦截器 过滤器
+ */
+@Configuration
+public class CustomerWebMvcConfigurer implements WebMvcConfigurer {
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    /**
+     * 注册拦截器
+     */
+    // 方式1
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        System.out.println("===========拦截器==============");
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/**");
+    }
+
+    /**
+     * fastjson转换器
+     * @param converters
+     */
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        System.out.println("-----------fast json-------------------");
+        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(
+                //  SerializerFeature.WriteNullListAsEmpty,//List字段如果为null,输出为[],而非null
+                SerializerFeature.WriteNullStringAsEmpty,//字符类型字段如果为null,输出为"",而非null
+                SerializerFeature.DisableCircularReferenceDetect,//消除对同一对象循环引用的问题,默认为false
+                //  SerializerFeature.WriteNullBooleanAsFalse//Boolean字段如果为null,输出为false,而非null
+                SerializerFeature.WriteMapNullValue//是否输出值为null的字段,默认为false
+        );
+        fastJsonHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
+        converters.add(fastJsonHttpMessageConverter);
+    }
+}
--- tutorial_springboot/src/main/java/com/tutorial/start/controller/LoginController.java
+++ tutorial_springboot/src/main/java/com/tutorial/start/controller/LoginController.java
@@ -1,9 +1,12 @@
 package com.tutorial.start.controller;
 
+import com.tutorial.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by Jimmy. 2018/3/30  14:14
@@ -19,9 +22,12 @@ public class LoginController {
 
     @RequestMapping("/login")
     public String login(@RequestParam(value = "userName")String userName,
-                        @RequestParam(value = "password")String password, Model model){
+                        @RequestParam(value = "password")String password, Model model, HttpServletRequest request){
         model.addAttribute("name", "world");
+        User user = new User();
+        user.setUserName("jimmy");
         if(userName.equals("jimmy") && password.equals("123456")){
+            request.getSession().setAttribute("session_user",user);
             return "welcome";
         }else{
             return "error";
