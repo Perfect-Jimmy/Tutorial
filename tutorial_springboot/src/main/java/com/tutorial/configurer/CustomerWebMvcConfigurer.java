8bbdfaa1f343c869dcd45affd3eeef03e4d6fdbc
jimmy <773792867@qq.com>
2018-04-03 11:43:13 +0800

    spring boot 2.0 AOP日志

--- tutorial_springboot/src/main/java/com/tutorial/configurer/CustomerWebMvcConfigurer.java
+++ tutorial_springboot/src/main/java/com/tutorial/configurer/CustomerWebMvcConfigurer.java
@@ -6,10 +6,12 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.tutorial.interceptor.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -47,6 +49,11 @@ public class CustomerWebMvcConfigurer implements WebMvcConfigurer {
                 //  SerializerFeature.WriteNullBooleanAsFalse//Boolean字段如果为null,输出为false,而非null
                 SerializerFeature.WriteMapNullValue//是否输出值为null的字段,默认为false
         );
+        //编码格式
+        List<MediaType> fastMediaTypes = new ArrayList<>();
+        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        //
+        fastJsonHttpMessageConverter.setSupportedMediaTypes(fastMediaTypes);
         fastJsonHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
         converters.add(fastJsonHttpMessageConverter);
     }
