359a915d11efc4780f9500e2af4aa64511ceca2a
jimmy <773792867@qq.com>
2018-05-21 18:07:39 +0800

    solr

---
+++ tutorial_solr/src/main/java/com/solr/configurer/SolrConfigurer.java
@@ -0,0 +1,12 @@
+package com.solr.configurer;
+
+/**
+ * Created by Jimmy. 2018/5/21  17:01
+ */
+//@Configuration
+public class SolrConfigurer {
+
+   /* public HttpSolrClient httpSolrClient(){
+
+    }*/
+}
--- tutorial_solr/src/test/java/com/solr/test/SolrTest.java
+++ tutorial_solr/src/test/java/com/solr/test/SolrTest.java
@@ -3,12 +3,19 @@ package com.solr.test;
 import com.solr.SolrStart;
 import com.solr.domain.ProgramSeries;
 import com.solr.service.ProgramSeriesService;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
 import java.util.Date;
 
 /**
@@ -19,6 +26,25 @@ import java.util.Date;
 public class SolrTest {
     @Autowired
     private ProgramSeriesService programSeriesService;
+
+    @Autowired
+    private SolrClient solrClient;
+
+    @Test
+    public void query() throws IOException, SolrServerException {
+        System.out.println(solrClient.getClass().toString());
+        SolrQuery query = new SolrQuery();// 查询
+        query.setQuery("*:*");
+        QueryResponse response = solrClient.query("programSeries",query);
+        SolrDocumentList solrDocumentList = response.getResults();
+        System.out.println(solrDocumentList.size());
+        for(SolrDocument sd : solrDocumentList){
+            System.out.println("solr获取值 id：" + sd.getFieldValue("id"));
+            System.out.println("solr获取值 name：" + sd.getFieldValue("name"));
+        }
+    }
+
+
 
     @Test
     public void save(){
