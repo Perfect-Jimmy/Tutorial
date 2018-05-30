08d912dea8c7f8bb5a2000a6730007c221dbfb9b
jimmy <773792867@qq.com>
2018-05-30 14:30:06 +0800

    spring boot 监听器

--- tutorial_springboot/src/main/java/com/tutorial/AppStart.java
+++ tutorial_springboot/src/main/java/com/tutorial/AppStart.java
@@ -19,6 +19,8 @@ public class AppStart {
        /* Binder binder = Binder.get(context.getEnvironment());
         List<CityParam> citys = binder.bind("spring.city", Bindable.listOf(CityParam.class)).get();
         System.out.println(citys);*/
+
+
     }
 
 }
